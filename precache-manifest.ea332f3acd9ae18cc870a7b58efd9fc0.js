fe6fef23a8fa46fd6d09f221bebedfdd6f00ddf5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-01 04:34:03 +0000

    Deploy STEP-tw/gol-heerajoshi to github.com/STEP-tw/gol-heerajoshi.git:gh-pages

---
+++ precache-manifest.ea332f3acd9ae18cc870a7b58efd9fc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b6bfb1a3eb03392b8f",
+    "url": "/gol-heerajoshi/static/js/runtime~main.76b6bfb1.js"
+  },
+  {
+    "revision": "5039bce4355f3e30e3e8",
+    "url": "/gol-heerajoshi/static/js/main.5039bce4.chunk.js"
+  },
+  {
+    "revision": "7cd5e93a1b07132ada70",
+    "url": "/gol-heerajoshi/static/js/1.7cd5e93a.chunk.js"
+  },
+  {
+    "revision": "5039bce4355f3e30e3e8",
+    "url": "/gol-heerajoshi/static/css/main.11c9bd96.chunk.css"
+  },
+  {
+    "revision": "b481f94374519c6ce321c9fbefb9f919",
+    "url": "/gol-heerajoshi/index.html"
+  }
+];
